5d5d0bab259d38b6213392ce4a599cc03092a9f4
DemianDD <thvarichdemyan@icloud.com>
2024-01-25 14:10:39 +0200

    add error component

---
+++ src/components/Error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorComponent = () => {
+    return (
+        <div className='error_container'>
+            <span className='text_style'>Incorrect file format</span>
+        </div>
+    )
+}
+
+export default ErrorComponent
